20439c5b34413261ffe709f5f88ba4f72ae77d59
ufo001jone@outlook.com <ufo010jone@outlook.com>
2024-12-24 07:25:33 +0800

    encoders

--- include/configuration.h
+++ include/configuration.h
@@ -8,18 +8,18 @@
 
 // MOTOR_PIN_LEFT - Left Motor Control Pins
 #define MOTOR_LEFT_EN 2    // Left motor enable pin
-#define MOTOR_LEFT_A_PIN 3 // Left motor direction A (PWM supported)
-#define MOTOR_LEFT_D_PIN 5 // Left motor direction D (PWM supported)
+#define MOTOR_LEFT_A_PIN 5 // Left motor direction A (PWM supported)
+#define MOTOR_LEFT_D_PIN 6 // Left motor direction D (PWM supported)
 
 // MOTOR_PIN_RIGHT - Right Motor Control Pins
-#define MOTOR_RIGHT_EN 4    // Right motor enable pin
-#define MOTOR_RIGHT_D_PIN 6 // Right motor direction D (PWM supported)
-#define MOTOR_RIGHT_A_PIN 9 // Right motor direction A (PWM supported)
+#define MOTOR_RIGHT_EN 3     // Right motor enable pin
+#define MOTOR_RIGHT_D_PIN 9  // Right motor direction D (PWM supported)
+#define MOTOR_RIGHT_A_PIN 10 // Right motor direction A (PWM supported)
 
 // PS2X_remote_player_controller - PS2 Controller Pin Definitions
 #define PS2_DAT 12 // Data pin of the PS2 controller
 #define PS2_CMD 11 // Command pin of the PS2 controller
-#define PS2_SEL 10 // Select pin of the PS2 controller
+#define PS2_SEL 7  // Select pin of the PS2 controller
 #define PS2_CLK 13 // Clock pin of the PS2 controller
 
 #endif
